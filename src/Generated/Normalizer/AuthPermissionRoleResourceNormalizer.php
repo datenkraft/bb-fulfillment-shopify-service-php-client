66e01ea337610629954a37e910610127b94206ae
CircleCI <office@datenkraft.com>
2023-03-13 15:27:53 +0000

    Autogenerated client update [publish]

---
+++ src/Generated/Normalizer/AuthPermissionRoleResourceNormalizer.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Datenkraft\Backbone\Client\FulfillmentShopifyService\Generated\Normalizer;
+
+use Jane\Component\JsonSchemaRuntime\Reference;
+use Datenkraft\Backbone\Client\FulfillmentShopifyService\Generated\Runtime\Normalizer\CheckArray;
+use Symfony\Component\Serializer\Exception\InvalidArgumentException;
+use Symfony\Component\Serializer\Normalizer\DenormalizerAwareInterface;
+use Symfony\Component\Serializer\Normalizer\DenormalizerAwareTrait;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareTrait;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+class AuthPermissionRoleResourceNormalizer implements DenormalizerInterface, NormalizerInterface, DenormalizerAwareInterface, NormalizerAwareInterface
+{
+    use DenormalizerAwareTrait;
+    use NormalizerAwareTrait;
+    use CheckArray;
+    public function supportsDenormalization($data, $type, $format = null) : bool
+    {
+        return $type === 'Datenkraft\\Backbone\\Client\\FulfillmentShopifyService\\Generated\\Model\\AuthPermissionRoleResource';
+    }
+    public function supportsNormalization($data, $format = null) : bool
+    {
+        return is_object($data) && get_class($data) === 'Datenkraft\\Backbone\\Client\\FulfillmentShopifyService\\Generated\\Model\\AuthPermissionRoleResource';
+    }
+    /**
+     * @return mixed
+     */
+    public function denormalize($data, $class, $format = null, array $context = array())
+    {
+        if (isset($data['$ref'])) {
+            return new Reference($data['$ref'], $context['document-origin']);
+        }
+        if (isset($data['$recursiveRef'])) {
+            return new Reference($data['$recursiveRef'], $context['document-origin']);
+        }
+        $object = new \Datenkraft\Backbone\Client\FulfillmentShopifyService\Generated\Model\AuthPermissionRoleResource();
+        if (null === $data || false === \is_array($data)) {
+            return $object;
+        }
+        if (\array_key_exists('permissionCode', $data)) {
+            $object->setPermissionCode($data['permissionCode']);
+        }
+        if (\array_key_exists('roleCode', $data)) {
+            $object->setRoleCode($data['roleCode']);
+        }
+        return $object;
+    }
+    /**
+     * @return array|string|int|float|bool|\ArrayObject|null
+     */
+    public function normalize($object, $format = null, array $context = array())
+    {
+        $data = array();
+        $data['permissionCode'] = $object->getPermissionCode();
+        $data['roleCode'] = $object->getRoleCode();
+        return $data;
+    }
+}
