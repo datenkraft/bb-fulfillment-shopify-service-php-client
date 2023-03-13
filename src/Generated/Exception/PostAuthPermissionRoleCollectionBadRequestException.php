d75aa3c11c6a7fb0cd00480b11b8d188f945eb1d
CircleCI <office@datenkraft.com>
2023-03-13 15:07:48 +0000

    Autogenerated client update [publish]

---
+++ src/Generated/Exception/PostAuthPermissionRoleCollectionBadRequestException.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Datenkraft\Backbone\Client\FulfillmentShopifyService\Generated\Exception;
+
+class PostAuthPermissionRoleCollectionBadRequestException extends BadRequestException
+{
+    /**
+     * @var \Datenkraft\Backbone\Client\FulfillmentShopifyService\Generated\Model\ErrorResponse
+     */
+    private $errorResponse;
+    public function __construct(\Datenkraft\Backbone\Client\FulfillmentShopifyService\Generated\Model\ErrorResponse $errorResponse)
+    {
+        parent::__construct('Bad Request');
+        $this->errorResponse = $errorResponse;
+    }
+    public function getErrorResponse() : \Datenkraft\Backbone\Client\FulfillmentShopifyService\Generated\Model\ErrorResponse
+    {
+        return $this->errorResponse;
+    }
+}
