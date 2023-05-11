08ba60a16d0dede75b213803f79fdfc1ac980466
CircleCI <office@datenkraft.com>
2023-05-11 14:59:53 +0000

    Autogenerated client update [publish]

---
+++ src/Generated/Exception/PatchAuthRoleNotFoundException.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Datenkraft\Backbone\Client\FulfillmentShopifyService\Generated\Exception;
+
+class PatchAuthRoleNotFoundException extends NotFoundException
+{
+    /**
+     * @var \Datenkraft\Backbone\Client\FulfillmentShopifyService\Generated\Model\ErrorResponse
+     */
+    private $errorResponse;
+    public function __construct(\Datenkraft\Backbone\Client\FulfillmentShopifyService\Generated\Model\ErrorResponse $errorResponse)
+    {
+        parent::__construct('Not Found');
+        $this->errorResponse = $errorResponse;
+    }
+    public function getErrorResponse() : \Datenkraft\Backbone\Client\FulfillmentShopifyService\Generated\Model\ErrorResponse
+    {
+        return $this->errorResponse;
+    }
+}
