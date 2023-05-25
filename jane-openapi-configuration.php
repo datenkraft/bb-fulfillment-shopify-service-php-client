3df664e78d28531f8c0374187085c69b28c239e9
Wolfgang <wolfgang.herka@datenkraft.com>
2023-05-25 09:18:31 +0200

    init names

--- jane-openapi-configuration.php
+++ jane-openapi-configuration.php
@@ -2,7 +2,7 @@
 
 return [
     'openapi-file' => __DIR__ . '/openapi.json',
-    'namespace' => 'Datenkraft\Backbone\Client\XxxApi\Generated',
+    'namespace' => 'Datenkraft\Backbone\Client\FulfillmentShopifyService\Generated',
     'directory' => __DIR__ . '/src/Generated',
     'throw-unexpected-status-code' => true,
 ];
--- src/Client.php
+++ src/Client.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-namespace Datenkraft\Backbone\Client\XxxApi;
+namespace Datenkraft\Backbone\Client\FulfillmentShopifyService;
 
 use Datenkraft\Backbone\Client\BaseApi\ClientFactory;
 use Datenkraft\Backbone\Client\BaseApi\Exceptions\AuthException;
 
-class Client extends \Datenkraft\Backbone\Client\XxxApi\Generated\Client
+class Client extends \Datenkraft\Backbone\Client\FulfillmentShopifyService\Generated\Client
 {
     /**
      * @param ClientFactory $clientFactory
@@ -17,7 +17,7 @@ class Client extends \Datenkraft\Backbone\Client\XxxApi\Generated\Client
      */
     public static function createWithFactory(ClientFactory $clientFactory, string $endpointUrl = null): self
     {
-        $endpointUrl = $endpointUrl ?? getenv('X_DATENKRAFT_XXX_API_URL') ?: null;
+        $endpointUrl = $endpointUrl ?? getenv('X_DATENKRAFT_FULFILLMENT_SHOPIFY_SERVICE_URL') ?: null;
         return $clientFactory->createClient(static::class, $endpointUrl);
     }
 }
